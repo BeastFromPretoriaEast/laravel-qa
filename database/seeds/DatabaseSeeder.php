33005bbbad6d0521a2e1f35e5511e37a5acf9bf9
Markus Botha <markusbotha@gmail.com>
2022-01-11 15:52:04 +0000

    done

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Answer;
+use App\Question;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,8 +16,12 @@ class DatabaseSeeder extends Seeder
         factory(App\User::class, 3)->create()->each(function($u) {
             $u->questions()
             ->saveMany(
-                factory(App\Question::class, rand(1,5))->make()
-            );
+                factory(Question::class, rand(1,5))->make()
+            )->each(function($q) {
+                $q->answers()->saveMany(
+                    factory(Answer::class, rand(1,5))->make()
+                );
+            });
         });
         
     }
